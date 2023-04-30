26b98df107ecd3d8b9ce87044d5e4b68794c63ed
Kanyao Han <kanyaoh2@illinois.edu>
2023-04-30 02:50:34 -0500

    Add files via upload

--- self.py
+++ self.py
@@ -1,8 +1,19 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
+
 data = pd.read_csv('check.csv')
-st.dataframe(ddd[ddd['year']>2018][['link', 'text', 'user_description', 'user_name', 'year']])
+data = data[data['year']>2018][['link', 'text', 'user_description', 'user_name', 'year']]
 
+def make_clickable(link):
+    # target _blank to open new window
+    # extract clickable text to display for your link
+    text = link.split('=')[0]
+    return f'<a target="_blank" href="{link}">{text}</a>'
 
+data['link'] = data['link'].apply(make_clickable)
 
+i = st.number_input('Insert a number', min_value = 1, format = '%i')
+
+st.write(data[i-1:i].to_html(escape=False, index=False), unsafe_allow_html=True)
+st.video('https://twitter.com/MasonJameson99/status/1549911015843803137/video/1')
 
