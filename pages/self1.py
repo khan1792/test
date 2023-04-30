0055ba1c52af4063f585f122a58da0326ddacddc
Kanyao Han <kanyaoh2@illinois.edu>
2023-04-30 05:21:37 -0500

    Update self1.py

--- pages/self1.py
+++ pages/self1.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 data = pd.read_csv('check1.csv')
 y = st.number_input('Insert a number', min_value = 2006, value = 2019, format = '%i')
+rem = st.text_input(label="remove words", value = "尋找所有人的匿名問題|国有资产|风在吼”)
 data = data[data['year']==y][['link', 'text', 'user_description', 'user_name', 'year']]
+data = data[~data['text'].str.contains(rem)]
 data.index = range(len(data))
 
 def make_clickable(link):
@@ -16,4 +18,4 @@ data['link'] = data['link'].apply(make_clickable)
 
 i = st.number_input('Insert a number', min_value = 1, format = '%i')
 
-st.write(data[(i-1)*5:i*5].to_html(escape=False, index=False), unsafe_allow_html=True)
+st.write(data[(i-1)*2:i*2].to_html(escape=False, index=False), unsafe_allow_html=True)
