b221f133e9fa659f25966e83a534ef19321882af
Kanyao Han <kanyaoh2@illinois.edu>
2023-04-30 05:44:32 -0500

    Update self1.py

--- pages/self1.py
+++ pages/self1.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 data = pd.read_csv('check1.csv')
 y = st.number_input('Insert a number', min_value = 2006, value = 2019, format = '%i')
-rem = st.text_input(label="remove words", value = '驻京企业|風在吼|义和团|李鸿章|清朝|尋找所有人的匿名問題|图文小说|国有资产|风在吼')
+rem = st.text_input(label="remove words", value = '中共|中国人民|驻京企业|風在吼|义和团|李鸿章|清朝|尋找所有人的匿名問題|图文小说|国有资产|风在吼')
 data = data[data['year']==y][['link', 'text', 'user_description', 'user_name', 'year']]
 data = data[~data['text'].str.contains(rem)]
 data.index = range(len(data))
