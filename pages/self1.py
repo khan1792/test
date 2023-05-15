f1354865c736d5521bdb3ec4773e02263cb5fc45
Kanyao Han <kanyaoh2@illinois.edu>
2023-05-15 18:42:51 -0500

    Update self1.py

--- pages/self1.py
+++ pages/self1.py
@@ -7,6 +7,7 @@ y = st.number_input('Insert a number', min_value = 2006, value = 2019, format = 
 rem = st.text_input(label="remove words", value = '共产党|中共|中国人民|驻京企业|風在吼|义和团|李鸿章|清朝|尋找所有人的匿名問題|图文小说|国有资产|风在吼')
 data = data[data['year']==y][['link', 'text', 'user_description', 'user_name', 'year']]
 data = data[~data['text'].str.contains(rem)]
+data['user_description'] = data['user_description'].apply(lambda x: str(x))
 keyword = st.text_input(label="words", value = '')
 data=data[data['user_description'].str.contains(keyword)]
 data.index = range(len(data))
